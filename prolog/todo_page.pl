4fa44020f4464598e33d9563c7d4cfd5ea4e1fb9
Anne Ogborn <annie66us@yahoo.com>
2019-02-17 18:35:02 +0530

    broken, pushing so nicoabie can work on it

--- prolog/todo_page.pl
+++ prolog/todo_page.pl
@@ -38,23 +38,22 @@ todo_page(State) -->
     vue_context(_{initial_state: State,
                   pengine_app_name: todo_app,
                   root_element_sel: "#app"},
-                div([id(app)],
-                    [div(class('parameters'),
-                         [label(["Start Date",
-                                 vue_input([type(date), model(start_day)])]),
-                          label(["End Date",
-                                 vue_input([type(date), model(end_day)])]),
-                          label(["Meals per day",
-                                 % XXX: some way to indicate that
-                                 % meals_per_day should be treated as
-                                 % a number?
-                                 vue_input([type(number), model(meals_per_day)])])]),
-                     div(class(meals), \meals),
-                     div(class(schedule),
-                         [h2("Schedule"),
-                          vue_button(click(rerun), "New Schedule"),
-                          \calendar])])).
+                     div(class(todos),
+                         [h2("To Do"),
+                          \todo_list,
+                          \add_todo])).
 
+todo_list -->
+    vue_html([vue_list(todo in items,
+                       li(class(todo),
+                           [$(todo.desc),
+                            input([type(submit), name(delete), value($(todo.id))])
+                           ])
+                      )]).
+
+add_todo --> html(p('someday the add')).
+
+/*
 meals -->
     vue_html([h2("Menu Options"),
               ul(vue_list(meal in meals,
@@ -100,3 +99,4 @@ slots(Entries) -->
     vue_html(vue_list(entry in Entries,
                       div(class('meal-slot'),
                           $('entry.name')))).
+*/
